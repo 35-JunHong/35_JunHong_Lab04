f6e4eba271038655aa54b92962724ddc0693ea19
JunHong <neojunhong2003@gmail.com>
2022-01-21 02:31:30 +0800

    Lab04_Level1

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    Rigidbody PlayerRigidBody;
+
+    public Text scoreTxt;
+    public int score = 0;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        PlayerRigidBody.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(score>=2)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            score++;
+            scoreTxt.text = "Score: " + score;
+        }
+
+        if(collision.gameObject.tag=="Hazard")
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
